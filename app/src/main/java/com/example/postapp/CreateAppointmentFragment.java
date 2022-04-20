a5abe8acc2194c2aeeaff9739c9247f3fe9fd582
Maria <maria.nicolaiciuc@yahoo.com>
2022-04-20 19:46:57 +0300

    DatePicker

--- app/src/main/java/com/example/postapp/CreateAppointmentFragment.java
+++ app/src/main/java/com/example/postapp/CreateAppointmentFragment.java
@@ -1,19 +1,119 @@
 package com.example.postapp;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 public class CreateAppointmentFragment extends Fragment {
+    private DatePickerDialog datePickerDialog;
+    private Button btnDate;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_createappointment, container, false);
+        initDatePicker();
+
+        View view = inflater.inflate(R.layout.fragment_createappointment, container, false);
+
+        btnDate = (Button) view.findViewById(R.id.btnDatePicker);
+        btnDate.setText(getTodaysDate());
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePicker();
+            }
+        });
+        // return inflater.inflate(R.layout.fragment_createappointment, container, false);
+        return view;
+    }
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                btnDate.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(getActivity(), style, dateSetListener, year, month, day);
+    }
+
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1) {
+            return "JAN";
+        }
+        if(month == 2) {
+            return "FEB";
+        }
+        if(month == 3) {
+            return "MAR";
+        }
+        if(month == 4) {
+            return "APR";
+        }
+        if(month == 5) {
+            return "MAY";
+        }
+        if(month == 6) {
+            return "JUN";
+        }
+        if(month == 7) {
+            return "JUL";
+        }
+        if(month == 8) {
+            return "AUG";
+        }
+        if(month == 9) {
+            return "SEP";
+        }
+        if(month == 10) {
+            return "OCT";
+        }
+        if(month == 11) {
+            return "NOV";
+        }
+        if(month == 12) {
+            return "DEC";
+        }
+
+        return "JAN";
+    }
+
+    public void openDatePicker() {
+        datePickerDialog.show();
     }
 }
